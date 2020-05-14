1c4f426c42d60c12d6469ca36392d14ae014af91
Mr.Flava <thatelitemaili33t@gmail.com>
2020-05-14 17:21:17 +0300

    Added API method and forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "e8a16cf3e04b48da54db2d22392ce2b1";
 
 class App extends React.Component {
+
+  gettingWeather = async () => {
+    const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
         <Info />
+        <Form />
+        <Weather />
       </div>
       );
   }
